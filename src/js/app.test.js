1a50cba5b66722cfa876a8bac1cab44ed2df14a4
solarlime <solarlime@outlook.com>
2019-12-27 15:16:33 +0300

    Task done!

---
+++ src/js/app.test.js
@@ -0,0 +1,18 @@
+import GameSavingLoader from './gameSavingLoader';
+
+test('Normal behaviour', (done) => {
+  const expected = JSON.stringify({
+    id: 9,
+    created: 1546300800,
+    userInfo: {
+      id: 1,
+      name: 'Hitman',
+      level: 10,
+      points: 2000,
+    },
+  });
+  GameSavingLoader.load().then((saving) => {
+    expect(saving).toEqual(expected);
+    done();
+  });
+});
